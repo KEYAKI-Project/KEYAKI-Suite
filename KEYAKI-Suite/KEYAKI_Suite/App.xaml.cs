c553e178c450c4e90abc33e3f4e9b5a1b7216018
Fumiya Kume <fumiya.kume@hotmail.com>
2017-04-13 20:02:48 +0900

    Add Mobile App Project

---
+++ KEYAKI-Suite/KEYAKI_Suite/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using KEYAKI_Suite.Views;
+using Xamarin.Forms;
+
+namespace KEYAKI_Suite
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
