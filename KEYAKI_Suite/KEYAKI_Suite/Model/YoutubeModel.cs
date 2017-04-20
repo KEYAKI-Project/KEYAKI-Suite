7b250c82e28173ed969fc8439f518291b773ec6c
Fumiya Kume <fumiya.kume@hotmail.com>
2017-04-20 14:14:15 +0900

    読み込めていないクラスを整理

--- KEYAKI_Suite/KEYAKI_Suite/Model/YoutubeModel.cs
+++ KEYAKI_Suite/KEYAKI_Suite/Model/YoutubeModel.cs
@@ -1,4 +1,5 @@
-﻿using KEYAKI_Suite.define;
+﻿
+using KEYAKI_Suite.define;
 using Microsoft.Practices.ObjectBuilder2;
 using Newtonsoft.Json;
 using Reactive.Bindings;
@@ -6,35 +7,35 @@ using HttpClient = System.Net.Http.HttpClient;
 
 namespace KEYAKI_Suite.Model
 {
-    public class YoutubeModel
-    {
-        public ReactiveCollection<Item> YoutubeCollection { get; set; } = new ReactiveCollection<Item>();
+	public class YoutubeModel
+	{
+		public ReactiveCollection<Item> YoutubeCollection { get; set; } = new ReactiveCollection<Item>();
 
-        public YoutubeModel()
-        {
-            GetData();
-        }
+		public YoutubeModel()
+		{
+			GetData();
+		}
 
-        private async void GetData()
-        {
-            using (var client = new HttpClient())
-            {
-                var result = await client.GetAsync(
-                    "https://www.googleapis.com/youtube/v3/search?maxResults=20&part=snippet&channelId=UCmr9bYmymcBmQ1p2tLBRvwg&key=AIzaSyCy34PAhxHZixbSsVkpqWfpOs18dd90FgY");
-                var json = await result.Content.ReadAsStringAsync();
-                var youtubedata = JsonConvert.DeserializeObject<YoutubeData>(json);
-                youtubedata.items.ForEach(item =>
-                {
-                    YoutubeCollection.Add(item);
-                });
-            }
-        }
-    }
+		private async void GetData()
+		{
+			using (var client = new HttpClient())
+			{
+				var result = await client.GetAsync(
+					"https://www.googleapis.com/youtube/v3/search?maxResults=20&part=snippet&channelId=UCmr9bYmymcBmQ1p2tLBRvwg&key=AIzaSyCy34PAhxHZixbSsVkpqWfpOs18dd90FgY");
+				var json = await result.Content.ReadAsStringAsync();
+				var youtubedata = JsonConvert.DeserializeObject<YoutubeData>(json);
+				youtubedata.items.ForEach(item =>
+				{
+					YoutubeCollection.Add(item);
+				});
+			}
+		}
+	}
 
-    public class YoutubeInfo
-    {
-        public string Title { get; set; }
-        public string Detail { get; set; }
-        public string ImagePath { get; set; }
-    }
+	public class YoutubeInfo
+	{
+		public string Title { get; set; }
+		public string Detail { get; set; }
+		public string ImagePath { get; set; }
+	}
 }
--- KEYAKI_Suite/KEYAKI_Suite/ViewModels/MainPageViewModel.cs
+++ KEYAKI_Suite/KEYAKI_Suite/ViewModels/MainPageViewModel.cs
@@ -3,64 +3,63 @@ using System.Reactive.Linq;
 using Prism.Mvvm;
 using Prism.Navigation;
 using KEYAKI_Suite.Model;
-using  System;
-using KEYAKI_Suite.define;
+using System;
 using KEYAKI_Suite.Views;
 using Reactive.Bindings;
-using Reactive.Bindings.Extensions;
 using Xamarin.Forms;
+using KEYAKI_Suite.define;
 
 namespace KEYAKI_Suite.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
-    {
-        private KEYAKINewsModel _keyakiNewsModel;
-        private YoutubeModel _youtubeModel;
-        private readonly INavigationService _navigationService;
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+		private KEYAKINewsModel _keyakiNewsModel;
+		private YoutubeModel _youtubeModel;
+		private readonly INavigationService _navigationService;
 
-        public ReactiveCommand<NewsData> NewsTappedEvent { get; set; } = new ReactiveCommand<NewsData>();
-        public ReactiveCommand<Item> YoutubeTapCommand { get; set; } = new ReactiveCommand<Item>();
-        public ReactiveCommand NavigateSettingPageCommand { get; set; } = new ReactiveCommand();
-        public ReactiveCollection<NewsData> NewsDatas { get; set; }
-        public ReactiveCollection<Item> Snippets { get; set; }
-        public MainPageViewModel(KEYAKINewsModel keyakiNewsModel, INavigationService navigationService, YoutubeModel youtubeModel)
-        {
-            _keyakiNewsModel = keyakiNewsModel;
-            _navigationService = navigationService;
-            _youtubeModel = youtubeModel;
+		public ReactiveCommand<NewsData> NewsTappedEvent { get; set; } = new ReactiveCommand<NewsData>();
+		public ReactiveCommand<Item> YoutubeTapCommand { get; set; } = new ReactiveCommand<Item>();
+		public ReactiveCommand NavigateSettingPageCommand { get; set; } = new ReactiveCommand();
+		public ReactiveCollection<NewsData> NewsDatas { get; set; }
+		public ReactiveCollection<Item> Snippets { get; set; }
+		public MainPageViewModel(KEYAKINewsModel keyakiNewsModel, INavigationService navigationService, YoutubeModel youtubeModel)
+		{
+			_keyakiNewsModel = keyakiNewsModel;
+			_navigationService = navigationService;
+			_youtubeModel = youtubeModel;
 
-            NewsDatas = _keyakiNewsModel.NewsDatas;
-            Snippets = _youtubeModel.YoutubeCollection;
+			NewsDatas = _keyakiNewsModel.NewsDatas;
+			Snippets = _youtubeModel.YoutubeCollection;
 
-            NavigateSettingPageCommand.Subscribe(o =>
-            {
-                navigationService.NavigateAsync(nameof(SettingPage));
-            });
+			NavigateSettingPageCommand.Subscribe(o =>
+			{
+				navigationService.NavigateAsync(nameof(SettingPage));
+			});
 
-            YoutubeTapCommand
-                .Where(o => Snippets.Count != 0)
-                .Where(snippet => snippet != null)
-                .Subscribe(snippet => Device.OpenUri(new Uri("https://www.youtube.com/watch?v=" + snippet.id.videoId)));
+			YoutubeTapCommand
+				.Where(o => Snippets.Count != 0)
+				.Where(snippet => snippet != null)
+				.Subscribe(snippet => Device.OpenUri(new Uri("https://www.youtube.com/watch?v=" + snippet.id.videoId)));
 
-            NewsTappedEvent
-                .Where(o => NewsDatas.Count != 0)
-                .Where(o => o != null)
-                .Subscribe(o => Device.OpenUri(new Uri(o.Link)));
-        }
+			NewsTappedEvent
+				.Where(o => NewsDatas.Count != 0)
+				.Where(o => o != null)
+				.Subscribe(o => Device.OpenUri(new Uri(o.Link)));
+		}
 
 
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
 
-        }
+		}
 
-        public void OnNavigatingTo(NavigationParameters parameters)
-        {
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
 
-        }
+		}
 
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-        }
-    }
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+		}
+	}
 }
