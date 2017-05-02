e27e8f9a802d3a485ba4614a4aac68744b9dfc7c
Fumiya Kume <fumiya.kume@hotmail.com>
2017-05-02 21:55:58 +0900

    Move View Model FIles

---
+++ KEYAKI_Suite.ViewModel/KEYAKIYoutubePageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Reactive.Linq;
+using KEYAKI_Suit.YoutubeService;
+using KEYAKI_Suite.UseCase;
+using Reactive.Bindings;
+using Xamarin.Forms;
+
+namespace KEYAKI_Suite.ViewModels
+{
+    public class KEYAKIYoutubePageViewModel : BindableBase
+    {
+        private readonly KeyakiYoutubeListUseCase KeyakiYoutubeListUseCase;
+
+        public ReactiveCommand<Item> YoutubeTapCommand { get; set; } = new ReactiveCommand<Item>();
+
+        public ReactiveCollection<Item> YoutubeItems { get; set; }
+
+        public KEYAKIYoutubePageViewModel(KeyakiYoutubeListUseCase keyakiYoutubeListUseCase)
+        {
+            KeyakiYoutubeListUseCase = keyakiYoutubeListUseCase;
+
+
+            YoutubeItems = KeyakiYoutubeListUseCase.YoutubeDatas;
+
+            KeyakiYoutubeListUseCase.FetchYoutubeData();
+
+            YoutubeTapCommand
+                .Where(o => YoutubeItems.Count != 0)
+                .Where(snippet => snippet != null)
+                .Subscribe(snippet => Device.OpenUri(new Uri("https://www.youtube.com/watch?v=" + snippet.id.videoId)));
+        }
+    }
+}
---
+++ KEYAKI_Suite.ViewModel/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace KEYAKI_Suite.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+        public MainPageViewModel()
+		{
+           
+		}
+        
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+		}
+	}
+}
