9b1278f98bf88e4db25da5065eba7bff55ca0bcc
Fumiya Kume <fumiya.kume@hotmail.com>
2017-05-06 20:22:09 +0900

    Setting up Prism/Unity Environment

---
+++ KEYAKI_Suite.Native.UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Windows.Mvvm;
+using Reactive.Bindings;
+
+namespace KEYAKI_Suite.Native.UWP.ViewModels
+{
+    public class MainPageViewModel:ViewModelBase
+    {
+        public ReactiveProperty<string> Greeting { get; set; } = new ReactiveProperty<string>("Hello");
+    }
+}
