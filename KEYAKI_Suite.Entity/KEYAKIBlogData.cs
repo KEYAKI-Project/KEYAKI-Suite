9703e3b06cb8e92e22e7a2b39a2cdd38a47aac4b
Fumiya Kume <fumiya.kume@hotmail.com>
2017-05-11 23:28:14 +0900

    Add Entity

---
+++ KEYAKI_Suite.Entity/KEYAKIBlogData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KEYAKI_Suite.KEYAKIBlogService
+{
+    public class KEYAKIBlogData
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string ImageURL { get; set; }
+        public string PostWriter { get; set; }
+        public DateTime PostDatetime { get; set; }
+    }
+}
---
+++ KEYAKI_Suite.Entity/NewsData.cs
@@ -0,0 +1,10 @@
+﻿namespace KEYAKI_Suite
+{
+    public class NewsData
+    {
+        public string ImagePath { get; set; }
+        public string Text { get; set; }
+        public string Detail { get; set; }
+        public string Link { get; set; }
+    }
+}
