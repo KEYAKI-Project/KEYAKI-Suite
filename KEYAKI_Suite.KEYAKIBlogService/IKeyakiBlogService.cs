7681e1c5502ee0b8809c185fecec8697df5a5013
Fumiya Kume <fumiya.kume@hotmail.com>
2017-05-13 22:59:25 +0900

    Using Entity

--- KEYAKI_Suite.KEYAKIBlogService/IKeyakiBlogService.cs
+++ KEYAKI_Suite.KEYAKIBlogService/IKeyakiBlogService.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using KEYAKI_Suite.Entity;
 
 namespace KEYAKI_Suite.KEYAKIBlogService
 {
     public interface IKeyakiBlogService
     {
-        Task<IEnumerable<KEYAKIBlogData>> GetBlogData(int pageNumber = 0, int ArticleNumber = 25);
+        Task<IEnumerable<Entity.KEYAKIBlogData>> GetBlogData(int pageNumber = 0, int ArticleNumber = 25);
     }
 }
