79cabea7a49cd77a87f140f1fb1989c94a6eea23
Fumiya Kume <fumiya.kume@hotmail.com>
2017-05-22 00:24:34 +0900

    Add youtube API

---
+++ KEYAKI-Suite.web/Controllers/YoutubeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KEYAKI_Suit.YoutubeService;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KEYAKI_Suite.web.Controllers
+{
+    [Route("api/[controller]")]
+    public class YoutubeController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<YoutubeData> Get()
+        {
+            return await (new YoutubeService.YoutubeService()).GetYoutubeDataAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
